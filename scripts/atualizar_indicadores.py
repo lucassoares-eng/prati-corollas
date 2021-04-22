857213c99c0793873652d2519c24aee990587dfe
lucas.soares <lucas_soares.eng@hotmail.com>
2021-04-22 09:52:05 -0300

    Add as an function

---
+++ scripts/atualizar_indicadores.py
@@ -0,0 +1,8 @@
+from pandas import read_csv
+import json
+
+def get_indicadores():
+    indicadores = read_csv('dados/indicadores.csv', sep= ';', encoding='latin 1')
+    indicadores = indicadores.to_json(orient='records', force_ascii= False)
+    
+    return indicadores

96dbb25e148697b82b55032ccb1a7b46abb6a28b
lucas.soares <lucas_soares.eng@hotmail.com>
2021-04-22 09:47:00 -0300

    Add as an function

---
+++ scripts/atualizar_arvore.py
@@ -0,0 +1,26 @@
+from pandas import read_csv, DataFrame
+import json
+
+def get_arvore(diretoria, gerencia):  
+    # carregar dados da árvore:
+    dfArvore = read_csv('dados/arvore.csv', sep= ';', encoding='latin 1')
+
+    # filtrar diretorias:
+    if diretoria == 'todos':
+        diretorias = dfArvore.loc[dfArvore.nivel=='diretoria', ['areaID','area', 'areaName']]
+    else:
+        diretorias = dfArvore.loc[dfArvore.area==diretoria, ['areaID','area', 'areaName']]
+    diretorias = diretorias.to_json(orient='records', force_ascii= False)
+
+    # filtrar gerências:
+    if gerencia == 'todos':
+        if diretoria == 'todos':
+            gerencias = dfArvore.loc[dfArvore.nivel=='gerencia', ['areaID','area', 'areaName']]
+        else:
+            gerencias = dfArvore.loc[dfArvore.nivel=='gerencia', ['areaID','area', 'areaName']]
+            gerencias = dfArvore.loc[(dfArvore.area.str.split('_').str[0]==diretoria)&(dfArvore.nivel=='gerencia'), ['areaID','area', 'areaName']]
+    else:
+        gerencias = dfArvore.loc[dfArvore.area==gerencia, ['areaID','area', 'areaName']]
+    gerencias = gerencias.to_json(orient='records', force_ascii= False)
+    
+    return diretorias, gerencias

52cb9d5aed7260a8da39e79cdcc50e74687c4719
lucas.soares <lucas_soares.eng@hotmail.com>
2021-04-22 16:28:46 -0300

    Updates

---
+++ scripts/carregar_filtros.js
@@ -0,0 +1,54 @@
+var areaID = location.hash.split('&')[0].slice(1)
+var nivel = location.hash.split('&')[1]
+
+function carregar_filtros(){
+    if (nivel=='grupo') {
+        abertura = 'diretorias'
+    } else {
+        document.querySelector('#icone-diretorias').style.display = "none";
+        if (nivel=='diretoria') {
+            abertura = 'gerencias'
+        } else {
+            document.querySelector('#icone-gerencias').style.display = "none";
+            abertura = 'indicadores'
+        }
+    }
+    /*filtro diretoria*/
+    let bx = document.querySelector('#select-bx-diretoria')
+    if (nivel=='grupo') {
+        let opt = document.createElement('option')
+        opt.value = 'todos'
+        opt.innerText = 'Diretoria (todos)'
+        bx.appendChild(opt)
+        for (let i in diretorias){
+            let opt = document.createElement('option')
+            opt.value = diretorias[i].areaID
+            opt.innerText = diretorias[i].areaName
+            bx.appendChild(opt)
+        }
+    } else {
+        let opt = document.createElement('option')
+        opt.value = diretorias[0].areaID
+        opt.innerText = diretorias[0].areaName
+        bx.appendChild(opt)
+    }
+    /*filtro gerencia*/
+    bx = document.querySelector('#select-bx-gerencia')
+    if (nivel!='gerencia') {
+        let opt = document.createElement('option')
+        opt.value = 'todos'
+        opt.innerText = 'Gerência (todos)'
+        bx.appendChild(opt)
+        for (let i in gerencias){
+            let opt = document.createElement('option')
+            opt.value = gerencias[i].areaID
+            opt.innerText = gerencias[i].areaName
+            bx.appendChild(opt)
+        }
+    } else {
+        let opt = document.createElement('option')
+        opt.value = gerencias[0].areaID
+        opt.innerText = gerencias[0].areaName
+        bx.appendChild(opt)
+    }
+}

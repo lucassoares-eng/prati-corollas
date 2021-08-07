21847261a9bb9030bc2ea2678f72533ec9e5a51a
lucas.soares <lucas_soares.eng@hotmail.com>
2021-08-07 16:39:20 -0300

    Updates pagina login

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
-import Login from '../components/login'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
@@ -10,7 +9,6 @@ const IndexPage = () => (
         <a>About</a>
       </Link>
     </p>
-    <Login/>
   </Layout>
 )
 
